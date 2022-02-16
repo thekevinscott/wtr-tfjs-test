532c5bc6e66d37a2c6b00adcc98168081045be1c
Kevin Scott <151596+thekevinscott@users.noreply.github.com>
2022-02-16 07:36:23 -0500

    esbuild as a plugin

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -1,16 +1,20 @@
-const rollupCommonjs = require('@rollup/plugin-commonjs');
-const { fromRollup } = require('@web/dev-server-rollup');
+const { esbuildPlugin } = require('@web/dev-server-esbuild');
 
-const commonjs = fromRollup(rollupCommonjs);
+// const rollupCommonjs = require('@rollup/plugin-commonjs');
+// const { fromRollup } = require('@web/dev-server-rollup');
+
+// const commonjs = fromRollup(rollupCommonjs);
 
 module.exports = {
   files: ['**/*.test.ts'],
   plugins: [
-    commonjs({
-      include: [
-        './node_modules/@tensorflow/**/*',
-      ],
-    }),
+    esbuildPlugin({ ts: true, target: 'auto' }),
+
+//     commonjs({
+//       include: [
+//         './node_modules/@tensorflow/**/*',
+//       ],
+//     }),
   ],
 };
 
